b5836e029ce0cf1127b14ffb3ccde9f730fe60b3
huzaifayofizza <huzaifafizza981770@gmail.com>
2024-09-13 11:22:02 +0500

    classroom

---
+++ onlineflutter/lib/main.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: unused_local_variable
+import 'package:flutter/material.dart';
+import 'package:online_flutter/pages/homepage.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // int days = 30;
+    // String name ="codepure";
+    // double pi = 3.14;
+    // bool isMale = true;
+    // num temp = 39.5;
+
+    // var day = "Thuesday";
+    // const pl = 3.14;
+    // final pu = 3.14;
+
+
+
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
